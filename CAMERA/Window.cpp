c0c9ed38be39bfa1901448079e67089186654353
daniel lopez <daniel.l.m.2442.123@gmail.com>
2018-08-04 12:40:03 +0200

    Key handle

--- CAMERA/Window.cpp
+++ CAMERA/Window.cpp
@@ -4,12 +4,18 @@ Window::Window()
 {
 	width = 800;
 	height = 600;
+
+	for (size_t i = 0; i < 1024; i++)
+		keys[i] = 0;
 }
 
 Window::Window(GLint windowWidth, GLint windowHeight)
 {
 	width = windowWidth;
 	height = windowHeight;
+
+	for (size_t i = 0; i < 1024; i++)
+		keys[i] = 0;
 }
 
 
@@ -50,6 +56,9 @@ int Window::Initialise()
 	// Set context for GLEW to use
 	glfwMakeContextCurrent(mainWindow);
 
+	// Handle key + Mouse Input
+	createCallbacks();
+
 	// Allow modern extension features
 	glewExperimental = GL_TRUE;
 
@@ -63,4 +72,30 @@ int Window::Initialise()
 
 	// Setup viewport size
 	glViewport(0, 0, bufferWidth, bufferHeight);
+
+	glfwSetWindowUserPointer(mainWindow, this); //SDL: SDL_GetMouseState(&x,&y);
+}
+
+void Window::createCallbacks()
+{
+	glfwSetKeyCallback(mainWindow, handleKeys);
+}
+
+void Window::handleKeys(GLFWwindow * window, int key, int code, int action, int mode)
+{
+	Window* theWindow = static_cast<Window*>(glfwGetWindowUserPointer(window));
+
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GL_TRUE);
+
+	if (key >= 0 && key < 1024) {
+		if (action == GLFW_PRESS) {
+			theWindow->keys[key] = true;
+			printf("Pressed: %d\n", key);
+		}
+		else if (action == GLFW_RELEASE) {
+			theWindow->keys[key] = false;
+			printf("Realease: %d\n", key);
+		}
+	}
 }
---
+++ CAMERA/Window.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <stdio.h>
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+class Window
+{
+public:
+	Window();
+	Window(GLint windowWidth, GLint windowHeight);
+	~Window();
+
+	int Initialise();
+
+	GLfloat getBufferWidth() { return bufferWidth; }
+	GLfloat getBufferHeight() { return bufferHeight; }
+
+	bool getShouldClose() { return glfwWindowShouldClose(mainWindow); }
+
+	void swapBuffers() { glfwSwapBuffers(mainWindow); }
+
+private:
+	GLFWwindow * mainWindow;
+
+	GLint width, height;
+	GLint bufferWidth, bufferHeight;
+
+	bool keys[1024]; 
+
+	void createCallbacks();
+	static void handleKeys(GLFWwindow* window, int key, int code, int action, int mode);
+};
+
